8531d7d00f3e49b9899a04fb6cc2f4999834b180
James Munns <james@onevariable.com>
2021-12-14 01:34:26 +0100

    Update docs. First publish

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# `defmt-rtt`
+# `defmt-bbq`
 
 > A generic [`bbqueue`] based transport for [`defmt`] log messages
 
@@ -10,9 +10,71 @@
 This crate stores the logged messages into a thread-safe FIFO queue, which can then be transferred
 across any medium, such as USB, RS-485, or other transports.
 
+Although this crate acts as a `global_logger` implementer for `defmt`, it still requires
+you to *do* something with the messages. This is intended to be a reusable building block
+for a variety of different transport methods.
+
+## Usage
+
+This crates requires it's users to perform the following actions:
+
+1. (optional): If you'd like to select a different queue size than the default
+  1024, you'll need to set the `DEFMT_BBQ_BUFFER_SIZE` environment variable at
+  build time to configure the size. e.g.: `DEFMT_BBQ_BUFFER_SIZE=4096 cargo build`.
+2. Prior to the first `defmt` log, the user MUST call `defmt_bbq::init()`, which will
+  initialize the logging buffer, and return the `Consumer` half of the queue, which
+  gives access to incoming logging messages
+3. The user must regularly drain the logged messages. If the queue is filled, any
+  additional bytes will be discarded, potentially corrupting (some) logging messages
+
+For more information on the Consumer interface, see the [Consumer docs] in the `bbqueue`
+crate documentation.
+
+[Consumer docs]: https://docs.rs/bbqueue/latest/bbqueue/struct.Consumer.html
+
+### Example
+
+```rust
+#[entry]
+fn main() {
+    // MUST be called before the first `defmt::*` call!
+    let mut consumer = defmt_bbq::init().unwrap();
+
+    loop {
+        defmt::println!("Hello, world!");
+
+        if let Some(grant) = consumer.read() {
+            // do something with `bytes`, like send
+            // it over a serial port..
+
+            // Then when done, make sure you release the grant
+            // to free the space for future logging.
+            let glen = grant.len();
+            grant.release(glen);
+        }
+    }
+}
+```
+
+## Default Feature(s)
+
+This crate has a single default feature, which enables the `encoding-rzcobs`
+feature of the `defmt` crate.
+
+It is **strongly recommended** to leave this feature enabled (and to
+use rzcobs encoding) when using this crate. If the buffer is ever overfilled,
+then the remaining bytes will be discarded, which will temporarily corrupt
+the message stream.
+
+Because rzcobs is delimited by zero bytes, it is possible to recover
+from this corruption, with the loss of a limited number of messages.
+When using the "raw" encoding, you MUST ensure that the buffer is
+**never** overfilled, as it will NOT be possible to recover from this
+error condition.
+
 ## Provenance
 
-This repository is a hard-fork of `defmt-rtt`, obtained from the [`defmt`] repository.
+This repository is a fork of `defmt-rtt`, obtained from the [`defmt`] repository.
 
 This repository was forked as of upstream commit `50e3db37d5429ed3344726f01e1bc4bf04902251`.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,100 @@
+//! # `defmt-bbq`
+//!
+//! > A generic [`bbqueue`] based transport for [`defmt`] log messages
+//!
+//! [`defmt`]: https://github.com/knurling-rs/defmt
+//! [`bbqueue`]: https://github.com/jamesmunns/bbqueue
+//!
+//! `defmt` ("de format", short for "deferred formatting") is a highly efficient logging framework that targets resource-constrained devices, like microcontrollers.
+//!
+//! This crate stores the logged messages into a thread-safe FIFO queue, which can then be transferred
+//! across any medium, such as USB, RS-485, or other transports.
+//!
+//! Although this crate acts as a `global_logger` implementer for `defmt`, it still requires
+//! you to *do* something with the messages. This is intended to be a reusable building block
+//! for a variety of different transport methods.
+//!
+//! ## Usage
+//!
+//! This crates requires it's users to perform the following actions:
+//!
+//! 1. (optional): If you'd like to select a different queue size than the default
+//!   1024, you'll need to set the `DEFMT_BBQ_BUFFER_SIZE` environment variable at
+//!   build time to configure the size. e.g.: `DEFMT_BBQ_BUFFER_SIZE=4096 cargo build`.
+//! 2. Prior to the first `defmt` log, the user MUST call `defmt_bbq::init()`, which will
+//!   initialize the logging buffer, and return the `Consumer` half of the queue, which
+//!   gives access to incoming logging messages
+//! 3. The user must regularly drain the logged messages. If the queue is filled, any
+//!   additional bytes will be discarded, potentially corrupting (some) logging messages
+//!
+//! For more information on the Consumer interface, see the [Consumer docs] in the `bbqueue`
+//! crate documentation.
+//!
+//! [Consumer docs]: https://docs.rs/bbqueue/latest/bbqueue/struct.Consumer.html
+//!
+//! ### Example
+//!
+//! ```rust,no_run
+//! #[entry]
+//! fn main() {
+//!     // MUST be called before the first `defmt::*` call!
+//!     let mut consumer = defmt_bbq::init().unwrap();
+//!
+//!     loop {
+//!         defmt::println!("Hello, world!");
+//!
+//!         if let Some(grant) = consumer.read() {
+//!             // do something with `bytes`, like send
+//!             // it over a serial port..
+//!
+//!             // Then when done, make sure you release the grant
+//!             // to free the space for future logging.
+//!             let glen = grant.len();
+//!             grant.release(glen);
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ## Default Feature(s)
+//!
+//! This crate has a single default feature, which enables the `encoding-rzcobs`
+//! feature of the `defmt` crate.
+//!
+//! It is **strongly recommended** to leave this feature enabled (and to
+//! use rzcobs encoding) when using this crate. If the buffer is ever overfilled,
+//! then the remaining bytes will be discarded, which will temporarily corrupt
+//! the message stream.
+//!
+//! Because rzcobs is delimited by zero bytes, it is possible to recover
+//! from this corruption, with the loss of a limited number of messages.
+//! When using the "raw" encoding, you MUST ensure that the buffer is
+//! **never** overfilled, as it will NOT be possible to recover from this
+//! error condition.
+//!
+//! ## Provenance
+//!
+//! This repository is a fork of `defmt-rtt`, obtained from the [`defmt`] repository.
+//!
+//! This repository was forked as of upstream commit `50e3db37d5429ed3344726f01e1bc4bf04902251`.
+//!
+//! ## License
+//!
+//! Licensed under either of
+//!
+//! - Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+//!   http://www.apache.org/licenses/LICENSE-2.0)
+//!
+//! - MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+//!
+//! at your option.
+//!
+//! ### Contribution
+//!
+//! Unless you explicitly state otherwise, any contribution intentionally submitted
+//! for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+//! licensed as above, without any additional terms or conditions.
+
 #![no_std]
 
 mod consts;
@@ -10,7 +107,9 @@ use core::{
 use cortex_m::{interrupt, register};
 use bbqueue::{BBBuffer, Consumer, Producer, GrantW};
 
-/// BBQueue buffer size. Default: 1024; can be customized by setting the `DEFMT_RTT_BUFFER_SIZE` environment variable.
+/// BBQueue buffer size. Default: 1024; can be customized by setting the
+/// `DEFMT_RTT_BUFFER_SIZE` environment variable.
+///
 /// Use a power of 2 for best performance.
 use crate::consts::BUF_SIZE;
 
@@ -93,10 +192,19 @@ impl UnsafeGrantW {
 
 unsafe impl Sync for UnsafeGrantW {}
 
+// The underlying byte storage containing the logs. Always valid
 static BBQ: BBBuffer<BUF_SIZE> = BBBuffer::new();
+
+// A tracking variable for ensuring state. Always valid.
 static BBQ_STATE: AtomicUsize = AtomicUsize::new(logstate::UNINIT);
 
+// The producer half of the logging queue. This field is ONLY
+// valid if `init()` has been called.
 static BBQ_PRODUCER: UnsafeProducer = UnsafeProducer::new();
+
+// An active write grant to a portion of the `BBQ`, obtained through
+// the `BBQ_PRODUCER`. This field is ONLY valid if we are in the
+// `INIT_GRANT` state.
 static BBQ_GRANT_W: UnsafeGrantW = UnsafeGrantW::new();
 
 mod logstate {
@@ -116,7 +224,17 @@ mod logstate {
     pub const INIT_GRANT: usize = 2;
 }
 
-
+/// Initialize the BBQueue based global defmt sink. MUST be called before
+/// the first `defmt` log, or the log will panic!
+///
+/// On the first call to this function, the Consumer end of the logging
+/// queue will be returned. On any subsequent call, an error will be
+/// returned.
+///
+/// For more information on the Consumer interface, see the [Consumer docs] in the `bbqueue`
+/// crate documentation.
+///
+/// [Consumer docs]: https://docs.rs/bbqueue/latest/bbqueue/struct.Consumer.html
 pub fn init() -> Result<Consumer<'static, BUF_SIZE>, bbqueue::Error> {
     let (prod, cons) = BBQ.try_split()?;
 
@@ -126,7 +244,6 @@ pub fn init() -> Result<Consumer<'static, BUF_SIZE>, bbqueue::Error> {
     unsafe {
         BBQ_PRODUCER.uc_mu_fp.get().write(MaybeUninit::new(prod));
     }
-
 
     // MUST be done LAST
     BBQ_STATE.store(logstate::INIT_IDLE, Ordering::Release);
@@ -151,9 +268,11 @@ unsafe impl defmt::Logger for Logger {
             panic!("defmt logger taken reentrantly")
         }
 
+        // Ensure the logger has been initialized, and no grant is active
+        assert_eq!(BBQ_STATE.load(Ordering::Relaxed), logstate::INIT_IDLE);
+
         // no need for CAS because interrupts are disabled
         TAKEN.store(true, Ordering::Relaxed);
-
         INTERRUPTS_ACTIVE.store(primask.is_active(), Ordering::Relaxed);
 
         // safety: accessing the `static mut` is OK because we have disabled interrupts.
@@ -169,7 +288,8 @@ unsafe impl defmt::Logger for Logger {
         // safety: accessing the `static mut` is OK because we have disabled interrupts.
         ENCODER.end_frame(do_write);
 
-        if let Some(grant) = unsafe { BBQ_GRANT_W.take() } {
+        // If a grant is active, take it and commit it
+        if let Some(grant) = BBQ_GRANT_W.take() {
             let offset = BBQ_GRANT_W.offset.load(Ordering::Relaxed);
             grant.commit(offset);
         }
@@ -187,9 +307,9 @@ unsafe impl defmt::Logger for Logger {
     }
 }
 
-fn do_write(bytes: &[u8]) {
-    let mut remaining = bytes;
-
+// Drain as many bytes to the queue as possible. If the queue is filled,
+// then any remaining bytes will be discarded.
+fn do_write(mut remaining: &[u8]) {
     while !remaining.is_empty() {
         if let Some(mut grant) = unsafe { BBQ_GRANT_W.take() } {
             let mut offset = BBQ_GRANT_W.offset.load(Ordering::Relaxed);
@@ -207,7 +327,8 @@ fn do_write(bytes: &[u8]) {
                 unsafe { BBQ_GRANT_W.put(grant) }
             }
         } else {
-            // Unable to obtain a grant. We're totally full
+            // Unable to obtain a grant. We're totally full. Sorry about
+            // the rest of those bytes
             return;
         }
     }
